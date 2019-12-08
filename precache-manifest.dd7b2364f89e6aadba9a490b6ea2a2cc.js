cf363ea1a1b8f447b2304ebeac69d80e95be225d
Chad Tao <chenchen408@gmail.com>
2019-12-08 10:35:49 -0800

    Updates

---
+++ precache-manifest.dd7b2364f89e6aadba9a490b6ea2a2cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f220e151cc5234a71502f77a22b189b",
+    "url": "/omdb_app/index.html"
+  },
+  {
+    "revision": "f58bbe0092dd0a1b9503",
+    "url": "/omdb_app/static/css/main.ab483bb0.chunk.css"
+  },
+  {
+    "revision": "3b4302a77fc109d7c3df",
+    "url": "/omdb_app/static/js/2.c723f54c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/omdb_app/static/js/2.c723f54c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f58bbe0092dd0a1b9503",
+    "url": "/omdb_app/static/js/main.9919f056.chunk.js"
+  },
+  {
+    "revision": "35d00e3565b351c921f4",
+    "url": "/omdb_app/static/js/runtime-main.829a6bcb.js"
+  }
+]);
